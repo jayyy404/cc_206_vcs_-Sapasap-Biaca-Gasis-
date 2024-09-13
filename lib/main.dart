2d4199a7ad56905aac494a4ff97ff8a271353c42
prhehns <pmalatuba@gmail.com>
2024-09-13 23:15:13 +0800

    Revert "iya kay prince v2"
    This reverts commit ed14945b6892543f834dc12c2e2341cc4a4863ed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 // { John Paul Sapasap} : { "the sun will rise again"}
 // {Joven Carl Rex P. Biaca}: { "I can resist everything except temptation"}
 // {Prince Alexander Malatuba}: { "I want miss Robin's lapdog"}
-// {Margaux Oriana Gasis}: { "Blessings wait for you" }
-//{Gem Win Cañete}: { "Basta Importante, Mahalaga"}
+
 import 'package:flutter/material.dart';
 
 void main() {
